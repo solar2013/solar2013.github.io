62abe95b26b18035a998cbc3e288e09f6ae992f1
Alex Solari <alexsanify@gmail.com>
2023-12-04 20:33:29 +0200

    part.

--- content/lazyload.js
+++ content/lazyload.js
@@ -1,18 +1,12 @@
 window.LazyLoadQueue = window.LazyLoadQueue || [];
-window.LazyLoadReady = window.LazyLoadReady || true;
 window.LazyLoadInterval = window.LazyLoadInterval || setInterval(() => {
-    if (!window.LazyLoadReady)
-        return;
-
     const topImage = window.LazyLoadQueue.shift();
 
     if (topImage && topImage.parentElement.style.display != 'none'){
         var imageUrl = topImage.dataset["src"];
-        window.LazyLoadReady = false;   
-        topImage.onload = () => window.LazyLoadReady = true;
         topImage.src = imageUrl;
     }
-}, 100);
+}, 0);
 
 window.LazyLoadInitializer = window.LazyLoadInitializer || (() => {
     if (document.body.style.display == 'none'){
