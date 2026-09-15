dfa3c2cc1eb4f9962787e90f18a243a91ac46010
Alex Solari <alexsanify@gmail.com>
2026-09-15 21:02:14 +0300

    Enhance single album view functionality and update styles for improved layout

--- content/single-album-view.js
+++ content/single-album-view.js
@@ -1,24 +1,34 @@
-window.applySingleAlbumView = window.applySingleAlbumView || (() => {
-    const hash = document.location.hash;
+window.applySingleAlbumView =
+    window.applySingleAlbumView ||
+    (() => {
+        const hash = document.location.hash;
 
-    if (hash) {
-        const elementId = document.location.hash.replace('#', '');
-        const targetCard = document.getElementById('#release_'+elementId);
-        const allCards = document.querySelectorAll('.card');
+        if (hash) {
+            const elementId = document.location.hash.replace('#', '');
+            const targetCard = document.getElementById('#release_' + elementId);
+            const allCards = document.querySelectorAll('.card');
 
-        allCards.forEach(card => {
-            if (card == targetCard){
-                const albumTitle = targetCard.querySelector('.card-title').innerText || '';
+            allCards.forEach((card) => {
+                if (card == targetCard) {
+                    const albumTitle =
+                        targetCard.querySelector('.card-title').innerText || '';
+                    const cardImage = targetCard.querySelector('.card-image');
 
-                document.title = "SOLAR*13 - "+albumTitle;
-                document.querySelector("#releases-content").className = "center";
-                card.className += " force-center";
+                    document.title = 'SOLAR*13 - ' + albumTitle;
+                    document.querySelector('#releases-content').className =
+                        'center';
+                    document.documentElement.classList.add('single-album-view');
+                    document.documentElement.style.setProperty(
+                        '--single-album-image',
+                        `url("${cardImage.src}")`
+                    );
+                    card.className += ' force-center';
 
-                return;
-            }
+                    return;
+                }
 
-            card.querySelector('img').src = '';
-            card.remove();
-        });
-    }
-});
+                card.querySelector('img').src = '';
+                card.remove();
+            });
+        }
+    });
